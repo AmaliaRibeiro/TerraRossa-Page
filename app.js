2759fea7f4ad684970f210ad2087be4433c40ae8
AmaliaRibeiro <amalita9421@gmail.com>
2022-12-26 14:24:28 -0300

    feat: add first changes

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path'); //con este modulo se unifican las rutas para identificarlas mejor
+const app = express();
+
+app.use(express.static('public')); //se tiene la carpeta public como recurso estático para poder consumirlo
+
+app.listen(3030, ()=>{
+    console.log('Servidor activo en el puerto 3030');
+})
+
+// Determinacion de ejs como view engine
+/*app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));*/
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+})
+
+app.get('/catalogue', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/catalogue.html'));
+})
+
+app.get('/allBlogs', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/allBlogs.html'));
+})
